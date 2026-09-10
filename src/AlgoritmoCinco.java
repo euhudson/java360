c7d61d6f08dcdbfec00e8cfd84c9d343041a0992
= <=>
2026-09-10 09:13:42 -0300

    Atualiza o Algotimo 5

--- src/AlgoritmoCinco.java
+++ src/AlgoritmoCinco.java
@@ -1,6 +1,6 @@
 public class AlgoritmoCinco {
     void main() {
-        int numero = Integer.parseInt(IO.readln());
+        int numero = Integer.parseInt(IO.readln("Digite o numero: "));
         IO.println(numero- 1);
         IO.println(numero + 1);
 
