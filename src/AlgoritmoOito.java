f2b0590fc2d4520f509977b5e5e8d2b00617f122
= <=>
2026-09-10 11:36:21 -0300

    AlgoritmoOito.java

---
+++ src/AlgoritmoOito.java
@@ -0,0 +1,18 @@
+public class AlgoritmoOito {
+    void main() {
+        //Colhe o valor do usuário
+        int valor1 = Integer.parseInt(IO.readln("Digite o primeiro valor: "));
+        int valor2 = Integer.parseInt(IO.readln("Digite o segundo valor: "));
+
+        // Realiza a soma
+        int soma = valor1 + valor2;
+
+        // Imprime o resultado
+        IO.println("Produto: " + soma );
+
+
+
+
+
+    }
+}
