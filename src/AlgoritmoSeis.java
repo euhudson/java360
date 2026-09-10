b803f6b9cc13cceedd0b2bc3433eb31691955868
= <=>
2026-09-10 10:49:08 -0300

    AlgoritmoSeis

---
+++ src/AlgoritmoSeis.java
@@ -0,0 +1,16 @@
+public class AlgoritmoSeis {
+    void main(){
+        String usuario = IO.readln("Qual é o seu nome: ");
+        String endereco = IO.readln("Olá " + usuario + ", Qual é o seu endereço?: ");
+        String telefone = IO.readln(usuario + " Por favor insira o seu telefone: ");
+        
+        
+        IO.println("Nome: " + usuario);
+        IO.println("endereco: " + endereco);
+        IO.println("telefone: " + telefone);
+
+
+
+    }
+    
+}
