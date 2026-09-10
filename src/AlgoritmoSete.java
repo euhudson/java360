011cfcb8bc55b66301f73544fda62e989a2408cf
= <=>
2026-09-10 11:06:23 -0300

    AlgoritmoSete.java

---
+++ src/AlgoritmoSete.java
@@ -0,0 +1,18 @@
+public class AlgoritmoSete {
+    void main() {
+        // Entrada dos numeros
+        int numero = Integer.parseInt(IO.readln("Digite um numero inteiro: "));
+        int numero2 = Integer.parseInt(IO.readln("Digite outro numero inteiro: "));
+        
+        // soma dos numeros
+        int soma = numero + numero2;
+
+        
+        // Impressão da soma 
+        IO.println("Soma: " + soma);
+
+
+
+
+    }
+}
