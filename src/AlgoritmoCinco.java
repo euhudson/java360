c57329049a9c79bef1a5ee6e2c197994a483c533
= <=>
2026-09-10 09:08:21 -0300

    Adicionar o algoritmo 5

---
+++ src/AlgoritmoCinco.java
@@ -0,0 +1,9 @@
+public class AlgoritmoCinco {
+    void main() {
+        int numero = Integer.parseInt(IO.readln());
+        IO.println(numero- 1);
+        IO.println(numero + 1);
+
+    }
+          
+}
