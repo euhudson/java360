0f54dbf1a81cb7359be6c30a5be0d1a842611e90
= <=>
2026-09-10 11:52:45 -0300

    AlgoritmoNove.java

---
+++ src/AlgoritmoNove.java
@@ -0,0 +1,15 @@
+public class AlgoritmoNove {
+    void main() {
+    // usuário insere o numero    
+    int numeroReal = Integer.parseInt(IO.readln("Digite um numero real: "));
+
+    // divisão do numero real
+    int resultado = numeroReal / 3;
+
+    IO.println(resultado);
+
+
+
+
+    }    
+}
