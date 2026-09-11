3a656723d67490db98e4304e46ed5a0c266e465f
= <=>
2026-09-11 09:12:37 -0300

    Adicionando o AlgoritmoDez.java

---
+++ src/AlgoritmoDez.java
@@ -0,0 +1,17 @@
+public class AlgoritmoDez {
+    void main() {
+       int valor1 = Integer.parseInt(IO.readln("Digite o primeiro valor: "));
+       int valor2 = Integer.parseInt(IO.readln("Digite o segundo valor: "));
+
+       int media = valor1 + valor2;
+       int resultadoFinal = media / 2; 
+       IO.println("A média dos valores é:  " + resultadoFinal);
+
+
+
+
+
+
+
+    }
+}
